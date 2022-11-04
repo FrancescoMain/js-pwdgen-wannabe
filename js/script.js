9183103cda3186374fea05c384ac51035b864910
Francesco <cesaranofrancescomain@gmail.com>
2022-11-04 15:21:59 +0100

    name error correct

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 // Chiedi all’utente il suo nome,
-const name = prompt("Come ti chiami?")
+const Name = prompt("Come ti chiami?")
 // poi chiedi il suo cognome,
 const surname = prompt("Qual è il tuo cognome?")
 // poi chiedi il suo colore preferito
 const color = prompt("Qual è il tuo colore preferito?")
 // Infine scrivi sulla pagina nomecognomecolorepreferito21
-document.getElementById("title").innerHTML = `La tua password sarà: ${name}${surname}${color}21`
+document.getElementById("title").innerHTML = `La tua password sarà: ${Name}${surname}${color}21`
 
 document.getElementById("title2").innerHTML = `Mi raccomando non mostrarla a nessuno`
